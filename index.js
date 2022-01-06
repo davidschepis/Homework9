5604f8954e8c1307d123bf0f6d2610e0e5a39ec9
David Schepis <davidschepis@gmail.com>
2022-01-06 00:32:50 -0700

    inquirer now works and prompts user for input and saves it

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // TODO: Include packages needed for this application
-var inquirer = require("inquirer");
-var generateMarkdown = require("./utils/generateMarkdown.js");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's name?",
+    "Please write a short description of your project",
+    "What kind of license should your project have?",
+    "What command should be run to install dependencies?",
+    "What command should be run to run tests?",
+    "What does the user need to know about using this repo?",
+    "What does the user need to know about contributing to the repo?"
 ];
 
 // TODO: Create a function to write README file
@@ -17,8 +25,71 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile("chabs.txt", "abkdjfldsjfklsdjfkldsjflsdjflsdjfsdkljflsdk");
+    getUserInput();
+}
+
+//This function prompts the user for information about their github project
+function getUserInput() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "userName",
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "email",
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "projectName",
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "description",
+        },
+        {
+            type: "list",
+            message: questions[4],
+            name: "username",
+            choices: ["Apache 2.0 License", "Boost", "BSD", "None"],
+        },
+        {
+            type: "input",
+            message: questions[5],
+            name: "installDep",
+        },
+        {
+            type: "input",
+            message: questions[6],
+            name: "runTests",
+        },
+        {
+            type: "input",
+            message: questions[7],
+            name: "userKnow",
+        },
+        {
+            type: "input",
+            message: questions[8],
+            name: "userContribute",
+        },
+    ]).then((response) => {
+        createData(response);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+//This function formats the data for the readme based on the user input
+const createData = data => {
+    console.log(data);
 }
 
 // Function call to initialize app
 init();
+
+
