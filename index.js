d2ebccb1e8833d82c259a933790ef9be9461b7ee
David Schepis <davidschepis@gmail.com>
2022-01-05 22:07:07 -0700

    writetofile function complete

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 // TODO: Include packages needed for this application
 var inquirer = require("inquirer");
-var generateMarkdown = require ("./utils/generateMarkdown.js");
+var generateMarkdown = require("./utils/generateMarkdown.js");
+var fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile(fileName, data, (error) =>
+        error ? console.error(error) : console.log(`Writing to ${fileName} was successful!`)
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-
+    writeToFile("chabs.txt", "abkdjfldsjfklsdjfkldsjflsdjflsdjfsdkljflsdk");
 }
 
 // Function call to initialize app
